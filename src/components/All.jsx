3123a455a392b8238ccb77e96a3acc563dc540bf
abimanyu1510 <asalabimanyu15@gmail.com>
2023-12-27 09:19:14 +0530

    Card size updated

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from './Card'
 
 function All({course}) {
-  return <> <div className="row row-cols-1 row-cols-md-3 g-4">
+  return <> <div className="row row-cols-1 row-cols-md-3 g-4" id="card5">
    {course.map((CardData,i)=>{
     return <Card CardData={CardData} key={i}/>
   })
